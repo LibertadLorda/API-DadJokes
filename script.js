936a830d3ec3962977aa819b50e80c3faae7b66b
LibertadLorda <libertadlorda@gmail.com>
2023-06-17 14:22:55 +0200

    Ready

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const jokeBtn = document.getElementById('jokeBtn');
 
 jokeBtn.addEventListener("click", generateJoke)
  
-// USING ASYNC/AWAIT
 async function generateJoke() {
   const config = {
     headers: {
@@ -11,28 +10,9 @@ async function generateJoke() {
     },
   };
 
-
-  const res = await fetch("https://icanhazdadjoke.com", config)
-  .then((res) => res.json())
-  //const data = await res.json()
+const res = await fetch("https://icanhazdadjoke.com", config)
+.then((res) => res.json())
   .then((data) => {
     jokeEl.innerHTML = data.joke;
 })
 }
-
-
-
-// USING .then()
-//ESTO YA ESTA UTILIZADO
-// function generateJoke() {
-//   const config = {
-//     headers: {
-//       Accept: 'application/json',
-//     },
-//   }
-
-//   >>fetch('https://icanhazdadjoke.com', config)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       jokeEl.innerHTML = data.joke
-//     })
